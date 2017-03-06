5775cf3846320e95aa6f98bb81032bd754b3f117
gliderforester <gliderforester@o2.pl>
2017-03-06 23:27:35 +0100

    countries

--- scripts.js
+++ scripts.js
@@ -1,21 +1,23 @@
-function Telefon(marka, cena, kolor, ekran, system) {
-	this.marka=marka;
-	this.cena=cena;
-	this.kolor=kolor;
-	this.ekran=ekran;
-	this.system=system;
+$(function () {
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var countriesList = $('#countries');
 
-}
-Telefon.prototype.printInfo = function() {
-	console.log('Marka telefonu to ' + this.marka + ', kolor to ' + this.kolor + ', a cena to ' + this.cena + ' PLN.');
-}
-Telefon.prototype.techInfo = function() {
-	console.log('Dane techniczne: ekran (' + this.ekran + '"); system operacyjny (' + this.system + ').');
-}
+	$('#search').click(searchCountries);
 
-var samsungGS6 = new Telefon('Samsung', 1800, 'czarny', 5.5 , 'Android 6.0');
-var iPhone6S = new Telefon('Apple', 2250, 'srebrny', 5.5, 'iOS 10.0');
-var onePlusone = new Telefon('OnePlus', 1300, 'srebrny', 5.5, 'Android 7.0' );
+	function searchCountries() {
+		var countryName = $('#country-name').val();
+		if (!countryName.length) countryName = 'Poland';
+		$.ajax({
+			url: url + countryName,
+			method: 'GET',
+			success: showCountriesList
+		});
+	}
 
-iPhone6S.printInfo();
-iPhone6S.techInfo();
+	function showCountriesList(resp) {
+		countriesList.empty();
+		resp.forEach(function (item) {
+			$('<li>').text(item.name + ' - (' + item.capital + ')').appendTo(countriesList);
+		});
+	}
+});
